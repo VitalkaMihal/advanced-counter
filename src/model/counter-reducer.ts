b12be33a1933c77be46cb46e2227127bc24aa602
VitalkaMihal <vitalimikhalkevich1986@gmail.com>
2025-08-04 22:57:23 +0300

    add setFunc

--- src/model/counter-reducer.ts
+++ src/model/counter-reducer.ts
@@ -27,16 +27,8 @@ export const counterReducer = (state: DataType = initialState, action: any): Dat
             return {...state, stateError: true}
         case 'resetCounter':
             return {...state, value: state.startValue, stateError: false}
-        case 'startValue':{
-            const newState = {...state}
-            return {...newState, startValue: action.startValue}
-        }
-        case 'maxValue':{
-            const newState = {...state}
-            return {...newState, maxValue: action.maxValue}
-        }
         case 'set': {
-            return {...state, maxValue: action.maxValue, startValue: action.startValue}
+            return {...state, maxValue: action.maxValue, startValue: action.startValue, value: action.startValue}
         }
         case 'stateIncorrect':
             return {...state, stateIncorrect: action.stateIncorrect}
